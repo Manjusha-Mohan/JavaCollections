4f4eee0da554aec6dc245e22bcda5bb1f6bbd718
Manjusha <manjushakmohan7@gmail.com>
2024-09-18 13:11:40 +0400

    Initial commit

---
+++ src/list/ArrayListExample.java
@@ -0,0 +1,11 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        //create list of strings
+        List<String> names = new ArrayList<>();
+    }
+}
